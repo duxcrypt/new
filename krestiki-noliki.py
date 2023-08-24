846fe4780c98eeeb9ae95512d0ebd25a5726dbdc
Vladyslav Borshchov <86555361+duxcrypt@users.noreply.github.com>
2023-08-24 14:06:22 +0300

    Create krestiki-noliki.py

---
+++ krestiki-noliki.py
@@ -0,0 +1,43 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board, player):
+    # Проверка строк, столбцов и диагоналей на наличие выигрышных комбинаций
+    for i in range(3):
+        if all([board[i][j] == player for j in range(3)]) or all([board[j][i] == player for j in range(3)]):
+            return True
+    if all([board[i][i] == player for i in range(3)]) or all([board[i][2 - i] == player for i in range(3)]):
+        return True
+    return False
+
+def is_board_full(board):
+    return all([cell != " " for row in board for cell in row])
+
+def main():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = "X"
+
+    while True:
+        print_board(board)
+        row = int(input(f"Игрок {current_player}, выберите строку (0, 1, 2): "))
+        col = int(input(f"Игрок {current_player}, выберите столбец (0, 1, 2): "))
+
+        if board[row][col] == " ":
+            board[row][col] = current_player
+            if check_winner(board, current_player):
+                print_board(board)
+                print(f"Игрок {current_player} победил!")
+                break
+            elif is_board_full(board):
+                print_board(board)
+                print("Ничья!")
+                break
+            else:
+                current_player = "O" if current_player == "X" else "X"
+        else:
+            print("Эта клетка уже занята. Попробуйте снова.")
+
+if __name__ == "__main__":
+    main()
